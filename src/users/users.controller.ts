77968aa57b187beb4b6ded4a120a9f0cf278119d
daniele deltoso <daniele.deltoso2@capgemini.com>
2023-09-07 10:24:12 +0200

    split auth e user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,56 +1,32 @@
 import {
   Body,
   Controller,
-  Post,
   Get,
   Patch,
   Delete,
   Param,
   Query,
   NotFoundException,
-  Session,
   UseGuards,
 } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
+
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
-import { AuthService } from './auth.service';
+
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
 import { AuthGuard } from '../guards/auth.guard';
 
-@Controller('auth')
+@Controller('users')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(
-    private usersService: UsersService,
-    private authService: AuthService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
   @Get('/whoami')
   @UseGuards(AuthGuard)
   whoAmI(@CurrentUser() user: User) {
-    return user;
-  }
-
-  @Post('/signout')
-  signOut(@Session() session: any) {
-    session.userId = null;
-  }
-
-  @Post('/signup')
-  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
-    const user = await this.authService.signup(body.email, body.password);
-    session.userId = user.id;
-    return user;
-  }
-
-  @Post('/signin')
-  async signin(@Body() body: CreateUserDto, @Session() session: any) {
-    const user = await this.authService.signin(body.email, body.password);
-    session.userId = user.id;
     return user;
   }
 
